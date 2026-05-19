41229c63e66d491a8877d2ddd35f25c98f2fd046
rerxyz_ <reynard16rande@gmail.com>
2026-05-19 15:57:26 +0700

    add: Pelanggan model

---
+++ PROJECTKPL.API/Models/Pelanggan.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROJECTKPL.API.Models
+{
+    public class Pelanggan
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Gender { get; set; } = string.Empty;
+
+        [Required]
+        public string NoTelp { get; set; } = string.Empty;
+
+        public int Umur { get; set; }
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+        // Navigasi ke pesanan milik pelanggan ini
+        public List<Pesanan> RiwayatPembelian { get; set; } = new();
+    }
+}
