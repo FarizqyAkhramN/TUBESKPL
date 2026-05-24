f4f9c61f7c2a2d257358bac5f568388eaf6a9b0c
RayyanMFPP <rayyantelyu@gmail.com>
2026-05-24 23:14:43 +0700

    add: ApiResponse generic

---
+++ PROJECTKPL.CONSOLE/Models/ApiResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PROJECTKPL.CONSOLE.Models
+{
+    // Generic wrapper untuk semua response dari API
+    // T bisa berupa: JsonElement, List<JsonElement>, string, dll
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+
+        public static ApiResponse<T> Ok(T data) => new()
+        {
+            Success = true,
+            Data = data
+        };
+
+        public static ApiResponse<T> Fail(string message) => new()
+        {
+            Success = false,
+            Message = message
+        };
+    }
+}
